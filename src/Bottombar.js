0767b5d25fc22ca381d7d7b8a4185b6370d22a63
Sathes Balasingam <sathes352@gmail.com>
2019-08-10 09:57:35 -0400

    PlanningSite

---
+++ src/Bottombar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Bottombar = () => {
+  return(
+    <div>
+    <p> Beginning </p>
+    </div>
+  );
+}
+
+export default Bottombar;
---
+++ src/Mainpart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mainpart = () => {
+  return(
+    <div>
+    <p> Beginning </p>
+    </div>
+  );
+}
+
+export default Mainpart;
